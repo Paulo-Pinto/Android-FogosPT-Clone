643fd4b122d2df26ca449f3398316c339d157bc7
Paulo-Pinto <paulopintodigital@gmail.com>
2022-06-02 13:12:40 +0100

    operations API call

---
+++ app/src/main/java/pt/ulusofona/deisi/cm2122/g21700980_21906966/ExtraAdapter.kt
@@ -0,0 +1,41 @@
+package pt.ulusofona.deisi.cm2122.g21700980_21906966
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import pt.ulusofona.deisi.cm2122.g21700980_21906966.databinding.ListItemFireBinding
+
+class ExtraAdapter(
+    private var items: List<FireUI> = listOf(),
+    private val onClick: (FireUI) -> Unit,
+    private val onLongClick: (FireUI) -> Boolean
+) : RecyclerView.Adapter<ExtraAdapter.ExtraViewHolder>() {
+//FragmentExtraBinding
+    class ExtraViewHolder(val binding: ListItemFireBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExtraViewHolder {
+        return ExtraViewHolder(
+            ListItemFireBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent, false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ExtraViewHolder, position: Int) {
+        val item = items[position]
+        holder.binding.fireId.text = "Fogo em ${item.parish} [${item.uuid.take(8)}]"
+        holder.binding.fireState.text = "Estado: ${item.state}"
+        holder.binding.firePlace.text = item.place
+        holder.itemView.setOnClickListener { onClick(item) }
+        holder.itemView.setOnLongClickListener { onLongClick(item) }
+    }
+
+    override fun getItemCount() = items.size
+
+    fun updateItems(items: List<FireUI>) {
+        this.items = items
+        notifyDataSetChanged()
+    }
+}
--- app/src/main/java/pt/ulusofona/deisi/cm2122/g21700980_21906966/ExtraFragment.kt
+++ app/src/main/java/pt/ulusofona/deisi/cm2122/g21700980_21906966/ExtraFragment.kt
@@ -4,17 +4,25 @@ import android.content.Context
 import android.graphics.Color
 import android.os.BatteryManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.google.gson.Gson
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import org.chromium.net.CronetEngine
 import org.chromium.net.UrlRequest
 import pt.ulusofona.deisi.cm2122.g21700980_21906966.databinding.FragmentExtraBinding
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
 
+private const val TAG = "Extra Fragment TAG"
 
 class ExtraFragment : Fragment() {
 
@@ -28,21 +36,10 @@ class ExtraFragment : Fragment() {
         Pair("Máximo", "#da291c"),
     )
     private var ctr = 0
+    private var list = listOf<Fire>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//API things
-//        val myBuilder = CronetEngine.Builder(context)
-//        val cronetEngine: CronetEngine = myBuilder.build()
-//        val executor: Executor = Executors.newSingleThreadExecutor()
-//        val requestBuilder = cronetEngine.newUrlRequestBuilder(
-//            "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,relativehumidity_2m,windspeed_10m",
-//            MyUrlRequestCallback(),
-//            executor
-//        )
-//
-//        val request: UrlRequest = requestBuilder.build()
-//        binding.submitter.text=request.toString()
     }
 
     override fun onCreateView(
@@ -69,6 +66,17 @@ class ExtraFragment : Fragment() {
         val risk = risks[0]
         binding.risk.text = "Risco ${risk.first}"
         binding.risk.setTextColor(Color.parseColor(risk.second))
+
+        getAllOperationsWs { list ->
+            println("FOOOOOOOOOOOR")
+            for (l in list) {
+                binding.fireId.text = l.uuid
+                binding.firePlace.text = l.district + l.county
+                binding.fireState.text = l.state
+            }
+        }
+
+        binding.firePlace.text = "fora"
     }
 
     override fun onResume() {
@@ -84,9 +92,49 @@ class ExtraFragment : Fragment() {
             val bm = requireContext().getSystemService(Context.BATTERY_SERVICE) as BatteryManager
             val batLevel = bm.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
             if (batLevel <= 20) {
-                val gray = Color.rgb(127,127,127)
+                val gray = Color.rgb(127, 127, 127)
                 binding.risk.setTextColor(gray)
             }
         }.also { runnable = it }, 20000)
     }
+
+    private fun getAllOperationsWs(callback: (List<Fire>) -> Unit) {
+        data class GetAllOperationsResponse(
+            val uuid: String,
+            val expression: String,
+            val result: Double,
+            val timestamp: Long
+        )
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val request: Request = Request.Builder()
+                .url("https://cm-calculadora.herokuapp.com/api/operations")
+                .addHeader(
+                    "apikey",
+                    "8270435acfead39ccb03e8aafbf37c49359dfbbcac4ef4769ae82c9531da0e17"
+                )
+                .build()
+
+            val response = OkHttpClient().newCall(request).execute().body
+            if (response != null) {
+                val responseObj =
+                    Gson().fromJson(response.string(), Array<GetAllOperationsResponse>::class.java)
+                        .toList()
+                callback(responseObj.map {
+                    Fire(
+                        uuid = it.uuid,
+                        district = it.expression,
+                        county = " -- ",
+                        parish = it.result.toString(),
+                        obs = it.timestamp.toString(),
+                        state = it.timestamp.toString(),
+                        submitter = Person("api", "0000"),
+                    )
+                })
+            }
+        }
+    }
+
 }
+
+// API KEY calculadora : 8270435acfead39ccb03e8aafbf37c49359dfbbcac4ef4769ae82c9531da0e17
